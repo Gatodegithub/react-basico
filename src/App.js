49881b8e6568e4d2be218b9fbff12157e4108d13
leandro <aldoleandrosaavedramorales@gmail.com>
2021-03-20 17:44:50 -0300

    react basico de jon terminado?

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ import AjaxHooks from "./components/AjaxHooks";
 import Referencias from "./components/Referencias";
 import HooksPersonalizados from "./components/HooksPersonalizados";
 import Formularios from "./components/Formularios";
+import Estilos from "./components/Estilos";
+import ComponentesEstilizados from "./components/ComponentesEstilizados";
 
 
 function App() {
@@ -95,6 +97,10 @@ function App() {
           <Referencias />
           <hr />
           <Formularios />
+          <hr />
+          <Estilos />
+          <hr />
+          <ComponentesEstilizados />
         </section>
       </header>
     </div>
